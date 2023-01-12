9b42fe25b76826aef04c5ccfe1073490beb0d8c9
sulfitra <nurjannasul@gmail.com>
2023-01-12 14:25:00 +0800

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  dev: process.env.NODE_ENV === 'DEV',
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
