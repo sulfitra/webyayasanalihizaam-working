7ab654456741733bbab68a4d0be330b3564dc5c3
sulfitra <nurjannasul@gmail.com>
2023-01-12 14:50:44 +0800

    halaman donasi

---
+++ pages/donasi.vue
@@ -0,0 +1,13 @@
+<template>
+    <div  scroll-behavior="smooth">
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DonasiPage'
+  }
+  </script>
