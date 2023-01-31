278bc00cb915f25637fcc462b0d084bb8cdc27e2
sulfitra <nurjannasul@gmail.com>
2023-01-31 15:08:13 +0800

    navbar adjustments

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="~/static/alihizaam.png" alt="">
+                <b-img fluid src="~/static/alihizaam.png" alt=""></b-img>
             </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
